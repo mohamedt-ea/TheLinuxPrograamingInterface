46528e506df3397b21b716d3e17be4afb33b1ae6
mohamedt-ea <mohamedt@eaenergy.us>
2022-10-13 09:58:26 +0200

    Upload seek io implementation

---
+++ code_registry/ch4/simple_seek_io/simple_seek_io.c
@@ -0,0 +1,158 @@
+/*******************************
+ * Description: 
+ * demonstrates the use of lseek() in conjunction with read() and write().
+ * The first command-line argument to this program is the name of a file to be 
+ * opened. 
+ * The remaining arguments specify I/O operations to be performed on the file. 
+ * Each of these operations consists of a letter followed by an associated value 
+ * (with no separating space):
+ *  - soffset: Seek to byte offset from the start of the file.
+ *  - rlength: Read length bytes from the file, starting at the current file offset, and
+ *    display them in text form.
+ *  - Rlength: Read length bytes from the file, starting at the current file offset, and
+ *    display them in hexadecimal.
+ *  - wstr: Write the string of characters specified in str at the current file offset.
+ * 
+ * *****************************/
+
+
+#include <sys/types.h>  /* Type definitions used by many programs */
+#include <stdio.h>      /* Standard I/O functions */
+#include <stdlib.h>     /* Prototypes of commonly used library functions,
+                           plus EXIT_SUCCESS and EXIT_FAILURE constants */
+#include <unistd.h>     /* Prototypes for many system calls */
+#include <errno.h>      /* Declares errno and defines error constants */
+#include <string.h>     /* Commonly used string-handling functions */
+#include <stdbool.h>    /* 'bool' type plus 'true' and 'false' constants */
+#include <fcntl.h>
+#include <ctype.h>
+
+#define GN_NONNEG       01      /* Value must be >= 0 */
+#define GN_GT_0         02      /* Value must be > 0 */
+
+                                /* By default, integers are decimal */
+#define GN_ANY_BASE   0100      /* Can use any base - like strtol(3) */
+#define GN_BASE_8     0200      /* Value is expressed in octal */
+#define GN_BASE_16    0400      /* Value is expressed in hexadecimal */
+
+/* Convert a numeric command-line argument ('arg') into a long integer,
+   returned as the function result. 'flags' is a bit mask of flags controlling
+   how the conversion is done and what diagnostic checks are performed on the
+   numeric result; see get_num.h for details.
+
+   'fname' is the name of our caller, and 'name' is the name associated with
+   the command-line argument 'arg'. 'fname' and 'name' are used to print a
+   diagnostic message in case an error is detected when processing 'arg'. */
+
+static long
+getNum(const char *fname, const char *arg, int flags, const char *name)
+{
+    long res;
+    char *endptr;
+    int base;
+
+    if (arg == NULL || *arg == '\0')
+        gnFail(fname, "null or empty string", arg, name);
+
+    base = (flags & GN_ANY_BASE) ? 0 : (flags & GN_BASE_8) ? 8 :
+                        (flags & GN_BASE_16) ? 16 : 10;
+
+    errno = 0;
+    res = strtol(arg, &endptr, base);
+    if (errno != 0)
+        gnFail(fname, "strtol() failed", arg, name);
+
+    if (*endptr != '\0')
+        gnFail(fname, "nonnumeric characters", arg, name);
+
+    if ((flags & GN_NONNEG) && res < 0)
+        gnFail(fname, "negative value not allowed", arg, name);
+
+    if ((flags & GN_GT_0) && res <= 0)
+        gnFail(fname, "value must be > 0", arg, name);
+
+    return res;
+}
+
+/* Convert a numeric command-line argument string to a long integer. See the
+   comments for getNum() for a description of the arguments to this function. */
+
+long
+getLong(const char *arg, int flags, const char *name)
+{
+    return getNum("getLong", arg, flags, name);
+}
+
+
+int main(int argc, char *argv[])
+{
+    size_t len;
+    off_t offset;
+    int fd, ap, j;
+    unsigned char *buf;
+    ssize_t numRead, numWritten;
+
+    if (argc < 3 || strcmp(argv[1], "--help") == 0)
+        printf("%s file {r<length>|R<length>|w<string>|s<offset>}...\n",
+                 argv[0]);
+
+    fd = open(argv[1], O_RDWR | O_CREAT,
+                S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+                S_IROTH | S_IWOTH);                     /* rw-rw-rw- */
+    if (fd == -1)
+        exit("open");
+
+    for (ap = 2; ap < argc; ap++) {
+        switch (argv[ap][0]) {
+        case 'r':   /* Display bytes at current offset, as text */
+        case 'R':   /* Display bytes at current offset, in hex */
+            len = getLong(&argv[ap][1], GN_ANY_BASE, argv[ap]);
+
+            buf = malloc(len);
+            if (buf == NULL)
+                exit("malloc");
+
+            numRead = read(fd, buf, len);
+            if (numRead == -1)
+                exit("read");
+
+            if (numRead == 0) {
+                printf("%s: end-of-file\n", argv[ap]);
+            } else {
+                printf("%s: ", argv[ap]);
+                for (j = 0; j < numRead; j++) {
+                    if (argv[ap][0] == 'r')
+                        printf("%c", isprint(buf[j]) ?  buf[j] : '?');
+                    else
+                        printf("%02x ", buf[j]);
+                }
+                printf("\n");
+            }
+
+            free(buf);
+            break;
+
+        case 'w':   /* Write string at current offset */
+            numWritten = write(fd, &argv[ap][1], strlen(&argv[ap][1]));
+            if (numWritten == -1)
+                exit("write");
+            printf("%s: wrote %ld bytes\n", argv[ap], (long) numWritten);
+            break;
+
+        case 's':   /* Change file offset */
+            offset = getLong(&argv[ap][1], GN_ANY_BASE, argv[ap]);
+            if (lseek(fd, offset, SEEK_SET) == -1)
+                exit("lseek");
+            printf("%s: seek succeeded\n", argv[ap]);
+            break;
+
+        default:
+            cmdLineErr("Argument must start with [rRws]: %s\n", argv[ap]);
+        }
+    }
+
+    if (close(fd) == -1)
+        exit("close");
+
+    exit(EXIT_SUCCESS);
+}
